bad595c2cf911f8320372e108bd5d10ca8f5a679
mariahacs10 <mlallencodes@gmail.com>
2024-05-10 15:22:59 -0700

    Added notes on how combinded

--- firstapp/src/main/java/com/example/cardatabase/firstapp/WebConfig.java
+++ firstapp/src/main/java/com/example/cardatabase/firstapp/WebConfig.java
@@ -5,14 +5,25 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//This annotation is used with Spring Framework to mark a class as a configuration class.
+//It signifies that this class provides bean definitions for the application.
 @Configuration
+
+//This annotation enables Cross-Origin Resource Sharing (CORS) for requests originating from the specified URL 
+//(http://192.168.68.115:3000 in this case). 
 @CrossOrigin("http://192.168.68.115:3000")
+//This interface provides methods for configuring various aspects of Spring MVC.
+//This class, WebConfig, implements this interface to provide its own configurations.
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
- 
-    }
+	 // This line comments out the @Override annotation
+	 // This annotation indicates that the following method is overriding
+	 // a method from the WebMvcConfigurer interface.
+	 @Override
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	     // This line is the  resource handling this method maps requests to static resources within the application.
+	      registry.addResourceHandler("/**")
+	             .addResourceLocations("classpath:/static/");
+	
+	 }
 }
