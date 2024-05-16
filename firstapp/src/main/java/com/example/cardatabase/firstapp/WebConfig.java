e118ec66d2b409dde482b36ca371205f99bf6cba
mariahacs10 <mlallencodes@gmail.com>
2024-05-16 13:42:52 -0700

    Updated notes, Also changed code

--- firstapp/src/main/java/com/example/cardatabase/firstapp/WebConfig.java
+++ firstapp/src/main/java/com/example/cardatabase/firstapp/WebConfig.java
@@ -13,23 +13,35 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //This annotation is used with Spring Framework to mark a class as a configuration class.
 //It signifies that this class provides bean definitions for the application.
-@Configuration
+//@Configuration
+//
+////This annotation enables Cross-Origin Resource Sharing (CORS) for requests originating from the specified URL 
+////(http://192.168.68.115:3000 in this case). 
+//@CrossOrigin("http://192.168.68.114:3000")
+////This interface provides methods for configuring various aspects of Spring MVC.
+////This class, WebConfig, implements this interface to provide its own configurations.
+//public class WebConfig implements WebMvcConfigurer {
+//
+//	 // This line comments out the @Override annotation
+//	 // This annotation indicates that the following method is overriding
+//	 // a method from the WebMvcConfigurer interface.
+//	 @Override
+//	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//	     // This line is the  resource handling this method maps requests to static resources within the application.
+//	      registry.addResourceHandler("/**")
+//	             .addResourceLocations("classpath:/static/");
+//	
+//	 }
+//}
 
-//This annotation enables Cross-Origin Resource Sharing (CORS) for requests originating from the specified URL 
-//(http://192.168.68.115:3000 in this case). 
-@CrossOrigin("http://192.168.68.114:3000")
-//This interface provides methods for configuring various aspects of Spring MVC.
-//This class, WebConfig, implements this interface to provide its own configurations.
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	 // This line comments out the @Override annotation
-	 // This annotation indicates that the following method is overriding
-	 // a method from the WebMvcConfigurer interface.
-	 @Override
-	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	     // This line is the  resource handling this method maps requests to static resources within the application.
-	      registry.addResourceHandler("/**")
-	             .addResourceLocations("classpath:/static/");
-	
-	 }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://192.168.68.114:3000") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
